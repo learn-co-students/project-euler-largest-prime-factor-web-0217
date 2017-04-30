837c0773d75c65b984399517c3fd667438747e15
github username <github email address>
2017-04-30 18:47:09 -0400

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,21 @@
 # Enter your procedural solution here!
+
+def is_prime(n)
+  tests = Array(2..Math.sqrt(n).floor)
+  tests = tests.select { |x| n % x === 0}
+  return true if tests.empty?
+  false
+end
+
+def largest_prime_factor(limit)
+  test = 3
+  out = 0
+  while test <= limit
+    if ((limit % test == 0)) && is_prime(test)
+      out = test
+      limit = limit/test
+    end
+    test +=1
+  end
+  out
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,22 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+  attr_accessor :number
+
+  def initialize(input)
+    @number = largest_prime_factor(input)
+  end
+
+  def largest_prime_factor(input)
+      test = 3
+      out = 0
+      while test <= input
+        if ((input % test == 0)) && is_prime(test)
+          out = test
+          input = input/test
+        end
+        test +=1
+      end
+      out
+
+  end
+end
