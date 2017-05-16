0a73e9de68ba5832fe7361c5bae5c549d4d35ff0
hwalborn <hwalborn@gmail.com>
2017-05-16 12:21:48 -0400

    I think I cheated

--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,11 @@
 # Enter your object-oriented solution here!
+require 'prime'
+class LargestPrimeFactor
+  def initialize(num)
+    @num = num
+  end
+
+  def number
+    Prime.prime_division(@num).max{|a,b| a[0] <=> b[0]}[0]
+  end
+end
