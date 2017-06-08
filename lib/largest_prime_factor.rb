0e71f32cfeff6add7e340504e0b237a0373b1731
fakebenjay <fakebenjay@gmail.com>
2017-06-08 01:59:14 -0400

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,21 @@
-# Enter your procedural solution here!
+def largest_prime_factor(input)
+  factor_array = [input]
+
+  factor_array.each do |f|
+    if factor_array.length > 1
+      factor_array.shift
+    end
+
+    n = 2
+
+    while f % n != 0
+      n += 1
+    end
+
+    if f / n != 1
+      factor_array.push(f/n)
+      factor_array.push(n)
+    end
+  end
+  factor_array.sort[-2]
+end
